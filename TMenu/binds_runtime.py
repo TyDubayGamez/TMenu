9d054bd9470ca0e7bff5ed2f1e9ab3dba8522d2b
Ty Dubay Gamez <nobodys2006@gmail.com>
2026-09-09 01:51:13 -0400

    going to replace

--- TMenu/binds_runtime.py
+++
@@ -1,8 +0,0 @@
-from binds_config import BindsConfig
-from binds_keyboard import KeyboardBindRouter
-
-
-class BindsRuntime:
-    def __init__(self, state):
-        self.config = BindsConfig.load()
-        self.keyboard_router = KeyboardBindRouter()
